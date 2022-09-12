e1d360c9fd0d0ab22f9f4e7ae7a40de49edcdfc9
fatihmaras <fatihmmaras@gmail.com>
2022-09-12 03:31:34 +0300

    Added multiplication table

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_CarpimTablosuOrnegi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int J = 1; J <= 10; J++)
+                {
+                    Console.WriteLine(i + "x" + J + "=" + (i * J));
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
